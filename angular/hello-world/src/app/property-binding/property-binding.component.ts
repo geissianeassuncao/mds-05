05c5c1c376ba0e4cc97998f4240548a1767df842
geissi <geissianeassuncao@gmail.com>
2025-09-09 11:34:55 -0400

    aula 09/09

---
+++ angular/hello-world/src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  imports: [],
+  templateUrl: './property-binding.component.html',
+  styleUrl: './property-binding.component.scss'
+})
+export class PropertyBindingComponent {
+
+  isDisable=true;
+
+}
